bb8803c2948e7f607c351971d592f8584b20a19f
sachinumans <sachinumans@gmail.com>
2022-06-01 17:23:47 +0200

    Added analytical 2.2

--- code files/optApprox.m
+++ code files/optApprox.m
@@ -1,4 +1,4 @@
-function [alpha, beta, err] = optApprox(vars)
+function [alpha, beta] = optApprox(vars)
 syms alph bet
 syms v
 c = vars.c; vmax = vars.v_max;
@@ -12,10 +12,28 @@ A2 = int(r2, v, alph, vmax); % Area 2
 
 A = A1 + A2; % Total area
 
-fA = matlabFunction(A, 'Vars', {symvar(A)}); % Make a function of the vector [alpha, beta]
+d_alph = diff(A, alph, 1); % deriv of A wrt to alpha
+d_bet = diff(A, bet, 1); % deriv of A wrt to beta
 
-[vals, err] = fmincon(fA, [vmax/2, c*(vmax/2)^2], [], []); % minimise
-alpha = vals(1);
-beta = vals(2);
+dd_alph = diff(A, alph, 2); % double deriv of A wrt to alpha
+dd_bet = diff(A, bet, 2); % double deriv of A wrt to beta
+
+[solAlph, solBet] = solve([d_alph == 0, d_bet == 0], [alph, bet]); % Find extrema of A
+solAlph = double(solAlph);
+solBet = double(solBet);
+
+idx = find(solAlph>0 & solAlph<vmax & solBet>0 & solBet<(c*vmax^2));
+
+alpha = solAlph(idx); % Grab valid values
+beta = solBet(idx);
+
+if 1==1
+    fA = matlabFunction(A); % Make a function of alpha, beta
+    figure()
+    fsurf(fA, [0 vmax 0 c*vmax^2])
+end
+
+val_dd_alph = double(subs(dd_alph, [alph, bet], [alpha, beta]));
+val_dd_bet = double(subs(dd_bet, [alph, bet], [alpha, beta]));
 
 end
