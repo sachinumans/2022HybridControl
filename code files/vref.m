fc21db34e895dcbcbccd7d2d70d347c94487d8cd
aronadam <64311624+aronadam@users.noreply.github.com>
2022-06-09 12:35:09 +0200

    vref functie

---
+++ code files/vref.m
@@ -0,0 +1,15 @@
+function [v_ref] = vref(alpha,t)
+%alpha en t erin; v_ref eruit
+if t<=3
+    v_ref=0.85*alpha;
+elseif t<=9
+    v_ref=1.2*alpha;
+elseif t<=15
+    v_ref=1.2*alpha-1/12*alpha*(t-9);
+elseif t<=18
+    v_ref=0.7*alpha;
+elseif t<=21
+    v_ref=0.7*alpha+4/15*alpha*(t-18);
+elseif t<=30
+    v_ref=0.9*alpha;
+end
